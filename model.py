28dd1dd2838b351512d7f6729d4d3aed2542e6e7
tordstr <tordstr@stud.ntnu.no>
2023-06-06 07:54:07 +0200

    tiny_shakespeare csv file added

--- model.py
+++ model.py
@@ -94,11 +94,12 @@ class gptModel(nn.Module):
 
     def generate(self, X, max_len=100):
         # block = X if len(X) < block_size else X[-block_size:]
+        # B = 1
         for _ in range(max_len):
             X_forward = X[:, -self.block_size:]
             logits, loss = self.forward(X_forward)
             logits = logits[:, -1, :]  # B, (last_char), probabilities
-            probs = F.softmax(logits, dim=-1)
+            probs = F.softmax(logits, dim=-1) # B, probabilities (1, probabilities)
             next = torch.multinomial(probs, num_samples=1)
             X = torch.cat((X, next), dim=1)
         return X
